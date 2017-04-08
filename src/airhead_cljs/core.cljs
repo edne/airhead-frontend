1e766b9f3411050ffe4af23863df8a22c02a96fe
Edoardo Negri <edne@gmx.com>
2017-04-09 01:03:23 +0200

    Playlist titles

--- src/airhead_cljs/core.cljs
+++ src/airhead_cljs/core.cljs
@@ -1,6 +1,7 @@
 (ns airhead-cljs.core
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [ajax.core :refer [GET POST]]))
 
 (def state (r/atom {:server "http://localhost:8080"}))
 
@@ -19,9 +20,22 @@
             :value (:server @state)
             :on-change change-server!}]])
 
+(defn playlist-div []
+  (let [playlist (r/atom [])
+        handler #(reset! playlist (% "items"))]
+    (GET "http://localhost:8080/api/queue"
+         {:handler handler
+          :response-format :json})
+    (fn []
+      [:div [:h2 "Playlist"]
+       [:ul (for [track @playlist]
+              [:li (track "title")])]])))
+
 (defn home-page []
   [:div [:h1 "Airhead"]
-   [server-form]])
+   [server-form]
+   [playlist-div]
+   ])
 
 ;; -------------------------
 ;; Initialize app
