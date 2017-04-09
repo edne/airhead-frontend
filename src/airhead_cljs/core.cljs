c9b2788cf3b68460bfafacfe5432123d4a9e68d7
Edoardo Negri <edne@gmx.com>
2017-04-09 03:02:00 +0200

    Enquque buttons

--- src/airhead_cljs/core.cljs
+++ src/airhead_cljs/core.cljs
@@ -1,7 +1,7 @@
 (ns airhead-cljs.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.core :as r]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET PUT]]))
 
 (def state (r/atom {:server "http://localhost:8080"
                     :playlist []
@@ -11,14 +11,18 @@
   (swap! state assoc k value))
 
 (defn get-playlist []
-  (GET "http://localhost:8080/api/queue"
+  (GET (str (@state :server) "/api/queue")
        {:handler #(update-state! :playlist (% "items"))
         :response-format :json}))
 
 (defn get-tracks []
-  (GET "http://localhost:8080/api/tracks"
+  (GET (str (@state :server) "/api/tracks")
        {:handler #(update-state! :tracks (% "items"))
         :response-format :json}))
+
+(defn enqueue-track [track]
+  (let [uuid (track "uuid")]
+    (PUT (str (@state :server) "/api/enqueue/" uuid))))
 
 ;; -------------------------
 ;; Views
@@ -39,11 +43,17 @@
    [:ul (for [track (@state :playlist)]
           [:li (track-span track)])]])
 
+(defn enqueue-button [track]
+  [:input {:type "button" :value "Enqueue"
+           :on-click #(enqueue-track track)}])
+
 (defn tracks-div []
   (js/setTimeout get-tracks 1000)
   [:div [:h2 "Tracks"]
    [:ul (for [track (@state :tracks)]
-          [:li (track-span track)])]])
+          [:li
+           (enqueue-button track)
+           (track-span track)])]])
 
 (defn home-page []
   [:div [:h1 "Airhead"]
