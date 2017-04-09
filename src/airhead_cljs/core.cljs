980c0cd2417c094b9d267766633dcf8c84a1d63d
Edoardo Negri <edne@gmx.com>
2017-04-09 02:41:01 +0200

    Add tracks section

--- src/airhead_cljs/core.cljs
+++ src/airhead_cljs/core.cljs
@@ -4,7 +4,8 @@
             [ajax.core :refer [GET POST]]))
 
 (def state (r/atom {:server "http://localhost:8080"
-                    :playlist []}))
+                    :playlist []
+                    :tracks []}))
 
 (defn update-state! [k value]
   (swap! state assoc k value))
@@ -12,6 +13,11 @@
 (defn get-playlist []
   (GET "http://localhost:8080/api/queue"
        {:handler #(update-state! :playlist (% "items"))
+        :response-format :json}))
+
+(defn get-tracks []
+  (GET "http://localhost:8080/api/tracks"
+       {:handler #(update-state! :tracks (% "items"))
         :response-format :json}))
 
 ;; -------------------------
@@ -33,11 +39,17 @@
    [:ul (for [track (@state :playlist)]
           [:li (track-span track)])]])
 
+(defn tracks-div []
+  (js/setTimeout get-tracks 1000)
+  [:div [:h2 "Tracks"]
+   [:ul (for [track (@state :tracks)]
+          [:li (track-span track)])]])
+
 (defn home-page []
   [:div [:h1 "Airhead"]
    [server-form]
    [playlist-div]
-   ])
+   [tracks-div]])
 
 ;; -------------------------
 ;; Initialize app
