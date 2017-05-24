3daceb527cc9462c5018810563e54c3d8ce37d77
Edoardo Negri <edne@gmx.com>
2017-05-24 14:00:28 +0200

    Show library

--- src/airhead_cljs/core.cljs
+++ src/airhead_cljs/core.cljs
@@ -7,34 +7,51 @@
 ; TODO: use data.json to get map keys as :keywords
 (def state (atom {:info {"name"          ""
                          "greet_message" ""
-                         "stream_url"    ""}}))
+                         "stream_url"    ""}
+                  :library {}}))
 
 ;; -------------------------
 ;; Views
 
-(defn info []
+(defn info-component []
   (let [title   (get-in @state [:info "name"])
         message (get-in @state [:info "greet_message"])
         url     (get-in @state [:info "stream_url"])]
-
     [:div.info
      [:h1 title]
      [:div.greet-message message]
      [:div.player [:audio {:src url
                            :controls "controls"}]]]))
 
-(defn page []
+(defn track-component [track]
+  [:span (str (track "artist") " - " (track "title"))])
+
+(defn library-component []
+  (let [tracks (-> @state
+                   (get-in [:library "tracks"])
+                   seq)]
+    [:div.library
+     [:h2 "Library"]
+     [:ul (for [[id track] tracks]
+            [:li [track-component track]])]]))
+
+(defn page-component []
   [:div.page
-   [info]])
+   [info-component]
+   [library-component]])
 
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (GET "/api/info" {:handler #(swap! state assoc :info %)
-                    :response-format :json})
+  (GET "/api/info"
+       {:handler #(swap! state assoc :info %)
+        :response-format :json})
+  (GET "/api/library"
+       {:handler #(swap! state assoc :library %)
+        :response-format :json})
 
-  (r/render [page] (.getElementById js/document "app")))
+  (r/render [page-component] (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
