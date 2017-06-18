f013da718a3501b3918978a1abda46a0e0d7671a
Tancredi Orlando <tancredi.orlando@gmail.com>
2017-06-18 12:55:39 +0200

    Major restyling.
    * Use pure.css components such as buttons, forms and inputs.
    * Use FontAwesome icons.
    * Restore upload button to make the user confirm the action.
    * Add a track counter in the library section.

--- src/airhead_frontend/components.cljs
+++ src/airhead_frontend/components.cljs
@@ -19,9 +19,11 @@
 
 (defn now-playing []
   (let [track (@app-state :now-playing)]
-    [:span (if track
-             (str (:artist track) " - " (:title track))
-             [:em "Nothing is playing"])]))
+    [:p#player-track
+      [:i.fa.fa-music]
+      [:span (if track
+               (str (:artist track) " - " (:title track))
+               [:em "Nothing is playing."])]]))
 
 (defn player-section []
   (let [audio-ref (r/atom nil)]
@@ -29,17 +31,25 @@
       (when-let [url (get-in @app-state [:info :stream_url])]
         [:section#player
 
-         [:audio {:ref #(reset! audio-ref %) :style {:display "none"}}
-          [:source {:src url}]]
+          [:audio {:ref #(reset! audio-ref %)}
+                  [:source {:src url}]]
 
-         [:span#controls
-          (when-let [audio @audio-ref]
-            (if (.-paused audio)
-              [:button {:on-click #(.play audio)}  "play"]
-              [:button {:on-click #(.pause audio)} "pause"]))]
+          [:div#player-controls.pure-button-group {:role "group"}
 
-         [now-playing]
-         [:a {:href url} "↗"]]))))
+            (when-let [audio @audio-ref]
+              (if (.-paused audio)
+                [:button.pure-button.pure-button-primary
+                  {:on-click #(.play audio)}
+                  [:i.fa.fa-play] "Play"]
+                [:button.pure-button.pure-button-primary.pure-button-active
+                  {:on-click #(.pause audio)}
+                  [:i.fa.fa-pause] "Pause"]))
+
+            [:a.pure-button {:href url}
+                            [:i.fa.fa-external-link]
+                            "Open stream"]]
+
+          [now-playing]]))))
 
 ;; -------------------------
 ;; Upload
@@ -47,7 +57,7 @@
 (defn progress-bar []
   (let [percentage (@app-state :upload-percentage)]
     (if (< 0 percentage 100)
-      [:progress {:max 100 :value percentage}])))
+      [:progress.pure-input-1 {:max 100 :value percentage}])))
 
 (defn upload-section []
   (let [form-ref (r/atom nil)]
@@ -55,13 +65,12 @@
       [:section#upload
        [:h2 "Upload"]
 
-       [:form {:ref #(reset! form-ref %)}
-        [:label "upload a file"
-         [:input {:type "file" :name "track"
-                  :on-change #(when-let [form @form-ref]
-                                (req/upload! form))
-                  :style {:display "none"}}]]
-        [progress-bar]]
+       [:form.pure-form {:ref #(reset! form-ref %)}
+         [:input.pure-input-3-4 {:type "file" :name "track"}]
+         [:input.pure-button..pure-button-primary.pure-input-1-4
+           {:type "button" :value "Upload"
+            :on-click #(when-let [form @form-ref] (req/upload! form))}]
+         [progress-bar]]
 
        [:p (@app-state :upload-status)]])))
 
@@ -69,14 +78,12 @@
 ;; Tracks
 
 (defn playlist-add-button [track]
-  [:button.add
-   {:on-click #(req/playlist-add! (:uuid track))}
-   "+"])
+  [:i.fa.fa-plus-square
+    {:on-click #(req/playlist-add! (:uuid track))}])
 
 (defn playlist-remove-button [track]
-  [:button.remove
-   {:on-click #(req/playlist-remove! (:uuid track))}
-   "-"])
+  [:i.fa.fa-minus-square
+    {:on-click #(req/playlist-remove! (:uuid track))}])
 
 (defn track-tr [track action-button]
   [:tr.track
@@ -92,12 +99,12 @@
   [:section#playlist
    [:h2 "Playlist"]
    (if-let [tracks (not-empty (@app-state :playlist))]
-     [:table.tracks
+     [:table.pure-table
       [:thead
        [:tr [:th] [:th  "Title"] [:th "Artist"] [:th "Album"]]]
       [:tbody (for [track tracks]
                 ^{:key track} [track-tr track playlist-remove-button])]]
-     "The playlist is empty")])
+     "The playlist is empty.")])
 
 ;; -------------------------
 ;; Search
@@ -105,15 +112,6 @@
 (defn on-query-change [e]
   (update-state! :query (-> e .-target .-value))
   (req/get-library!))
-
-(defn search-form []
-  [:section#search
-   [:form
-    [:label {:for "query"} "Search:"]
-    [:input {:type "text"
-             :id "query"
-             :value (@app-state :query)
-             :on-change on-query-change}]]])
 
 ;; -------------------------
 ;; Library
@@ -140,26 +138,33 @@
 
 (defn library-section []
   (let [tracks (@app-state :library)]
-    [:section#library.pure-u-1-2
-     [:h2 "Library"]
-     [search-form]
-     [:table.tracks
-      [:thead
-       [:tr [:th]
-        [sorting-th :title "Title"]
-        [sorting-th :artist "Artist"]
-        [sorting-th :album "Album"]]]
-      [:tbody (for [track (sort-tracks tracks)]
-                ^{:key track} [track-tr track playlist-add-button])]]]))
+    [:section#library
+      [:h2 "Library"]
+      [:form#library-search.pure-form
+        [:input.pure-input-3-4 {:type "text"
+                           :id "query"
+                           :placeholder "Search trough the library..."
+                           :value (@app-state :query)
+                           :on-change on-query-change}]
+        [:span.pure-input-1-4 (str "Tracks: " (count tracks))]]
+      [:table.pure-table.pure-table-striped
+        [:thead
+          [:tr [:th]
+               [sorting-th :title "Title"]
+               [sorting-th :artist "Artist"]
+               [sorting-th :album "Album"]]]
+        [:tbody (for [track (sort-tracks tracks)]
+                  ^{:key track} [track-tr track playlist-add-button])]]]))
 
 ;; -------------------------
 ;; Main
 
 (defn page-component []
- [:main
-   [:div.pure-u-1-2
+ [:main.pure-g
+   [:div.pure-u-1.pure-u-md-1-2
      [header]
      [player-section]
      [upload-section]
      [playlist-section]]
-   [library-section]])
+   [:div.pure-u-1.pure-u-md-1-2
+     [library-section]]])
